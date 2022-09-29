ef8f5dea3cee226a79885b3c9bc1f94bac3a27e5
zm0201 <73774019+zm0201@users.noreply.github.com>
2022-09-29 22:33:40 +0800

    添加数组合并
    添加list1,list2合并到arr中

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
 
 let sum =0
-for(let i=0; i<101;i++){
+for(let i=1; i<101;i++){
   sum=sum+i
 }
 console.log(sum)
+
+let list1=[1,2,3]
+let list2=[4,5,6]
+let arr=[...list1,...list2]
+console.log(arr)
